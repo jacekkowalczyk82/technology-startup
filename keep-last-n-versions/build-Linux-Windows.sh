6929d75abd2fcde59f326aff144101a4fc6aed1f
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2022-12-02 15:47:14 +0100

    added binaries for linux and windows amd64

--- keep-last-n-versions/build-Linux-Windows.sh
+++ keep-last-n-versions/build-Linux-Windows.sh
@@ -3,7 +3,7 @@ operating_systems=(linux windows)
 
 for os in ${operating_systems[@]}
 do
-    env GOOS=${os} GOARCH=amd64 go build -o keep-n-versions_${os}-amd64.bin keep-n-versions.go
+    env GOOS=${os} GOARCH=amd64 go build -o keep-last-n-versions_${os}-amd64.bin keep-n-versions.go
 done
 
 ls -alh 
