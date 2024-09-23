ae5dcd740a887b151ce544b57d241b502ea7aa41
Jacek Kowalczyk <jacekkowalczyk82@gmail.com>
2024-09-23 11:15:43 +0200

    added Base64 python encoding decoding script

---
+++ base64_python/base64_main.py
@@ -0,0 +1,80 @@
+# This is a sample Python script.  
+  
+# Press Shift+F10 to execute it or replace it with your code.  
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.  
+import base64  
+  
+TEST_STRING = "this is test text to base64 encoding"  
+  
+  
+def print_hi(name):  
+    # Use a breakpoint in the code line below to debug your script.  
+    print(f'Hi, {name}') # Press Ctrl+F8 to toggle the breakpoint.  
+  
+def encode(sample_string) :  
+    # sample_string = "GeeksForGeeks is the best"  
+    sample_string_bytes = sample_string.encode("UTF-8")  
+  
+    base64_bytes = base64.b64encode(sample_string_bytes)  
+    base64_string = base64_bytes.decode("UTF-8")  
+  
+    print(f"{sample_string} as Base64 encoded string: {base64_string}")  
+    return base64_string  
+  
+def decode(base64_string):  
+    # base64_string = " R2Vla3NGb3JHZWVrcyBpcyB0aGUgYmVzdA =="  
+    base64_bytes = base64_string.encode("UTF-8")  
+  
+    decoded_string_bytes = base64.b64decode(base64_bytes)  
+    decoded_string = decoded_string_bytes.decode("UTF-8")  
+  
+    print(f"Base64: {base64_string} Decoded: {decoded_string}")  
+    return decoded_string  
+
+def encode_file(file_path):
+    with  open(file_path, "rb") as  imagefile:
+        convert  =  base64.b64encode(imagefile.read())
+        print(convert.decode('utf-8'))
+        save_to_file(file_path+  "_base64.txt", convert.decode('utf-8')) 
+
+    print("encode file: " + file_path + " DONE")
+  
+def save_to_file(file_path, content):
+    file  =  open(file_path, mode="w")
+    file.write(content)
+    file.close()
+    print("save_to_file "+ file_path + " DONE")
+
+def decode_file(base64_file):
+    
+    output_zip = base64_file + '_decoded.bin'
+
+    # Step 1: Read the Base64 content
+    with open(base64_file, 'r') as file:
+        base64_content = file.read()
+
+    # Step 2: Decode the Base64 content
+    decoded_data = base64.b64decode(base64_content)
+
+    # Step 3: Save the decoded content as a ZIP file
+    with open(output_zip, 'wb') as zip_file:
+        zip_file.write(decoded_data)
+
+    print(f"Decoded data saved to {output_zip}")
+
+
+    print("decode_file: " + base64_file + " DONE")
+
+def save_to_binary_file(file_path, content):
+    file  =  open(file_path, mode="wb")
+    file.write(content)
+    file.close()
+    print("save_to_binary_file "+ file_path + " DONE")
+
+# Press the green button in the gutter to run the script.  
+if __name__ == '__main__':  
+    print_hi('PyCharm')  
+    encode(TEST_STRING)
+    encode_file("/home/jacek/Downloads/deck.js-latest.zip")
+    decode_file("/home/jacek/Downloads/deck.js-latest.zip_base64.txt")
+
